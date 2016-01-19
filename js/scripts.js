649678c40043b37dd9b49669019e6f1879e4189d
Oskar Radon and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-19 14:19:26 -0800

    changed first spec for not a triangle

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var triangle = function(sideOne, sideTwo, sideThree) {
+  if (sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne +sideTwo))
+  return true;
+  // (sideOne === sideTwo {
+  //   return "equilateral";
+  // }
+
+  };
+
+
+
+
+
+
+
+// (
+//   return false;
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,10 @@
 describe("triangle", function() {
-  it("is false for three values where the sum of two is less or equal to the third", function(){
-    expect(2, 2, 4).to.equal(false);
+  it("is true for three values where the sum of two is greater or equal to the third", function(){
+    expect(triangle(1, 2, 3)).to.equal(true);
   });
+
+  // it("is an equilateral triangle", function() {
+  //   expect(2, 2, 2).to.equal("equilateral");
+  // });
+
 });
