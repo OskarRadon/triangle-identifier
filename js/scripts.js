a3b04ac7ffd4bb2c9e96a6ef49b805eb3c20b3d5
Oskar Radon and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-19 15:25:11 -0800

    Add HTML form and CSS to hide output div

--- js/scripts.js
+++ js/scripts.js
@@ -13,24 +13,3 @@ var triangle = function(sideOne, sideTwo, sideThree) {
       return "not a triangle";
     }
   };
-
-// var triangle = function(sideOne, sideTwo, sideThree){
-//   if (sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)) {
-//     if (sideOne===sideTwo && sideTwo===sideThree) {
-//       return "equilateral";
-//     } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo) {
-//       return "isosceles";
-//     } else {
-//       return "scalene";
-//     }
-//   } else {
-//     return "These sides do not a triangle make.";
-//   }
-// };
-
-
-
-
-
-// (
-//   return false;
