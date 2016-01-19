caeaf737135b8150214b808530a436eda1ff56ab
Oskar Radon and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-19 14:29:24 -0800

    Add spec for isoscles triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 var triangle = function(sideOne, sideTwo, sideThree) {
-  if (sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne +sideTwo))
-  return true;
+  // if (sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne +sideTwo))
+
+    if (sideOne === sideTwo && sideTwo === sideThree) {
+    return "equilateral";
+  } else if (sideOne === sideTwo || sideTwo === sideThree){
+    return "isosceles"
+  };
   // (sideOne === sideTwo {
   //   return "equilateral";
   // }
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,15 @@
 describe("triangle", function() {
-  it("is true for three values where the sum of two is greater or equal to the third", function(){
-    expect(triangle(1, 2, 3)).to.equal(true);
+  // it("is true for three values where the sum of two is greater or equal to the third", function(){
+  //   expect(triangle(1, 2, 3)).to.equal(true);
+  // });
+
+  it("is an equilateral triangle", function() {
+    expect(triangle(2, 2, 2)).to.equal("equilateral");
   });
 
-  // it("is an equilateral triangle", function() {
-  //   expect(2, 2, 2).to.equal("equilateral");
-  // });
+  it("is an isosceles triangle", function() {
+    expect(triangle(2, 2, 4)).to.equal("isosceles");
+
+  });
 
 });
